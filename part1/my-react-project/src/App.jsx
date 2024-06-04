fb1fa86cc01ffb831d25a1c97331ff3cd2ac5c27
adnielys <129220133+adnielys@users.noreply.github.com>
2024-06-04 15:03:35 -0400

    all exercises from 1.6 to 1.11
    all exercises from 1.6 to 1.11

--- part1/my-react-project/src/App.jsx
+++ part1/my-react-project/src/App.jsx
@@ -1,66 +1,96 @@
 import React from 'react'
 import { useState } from 'react'
 
+const StatisticLine = ({text,value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
+const Statistics = ({good,neutral,bad}) => {
+  const total = (bad + good + neutral)
+  const average = total> 0 ? (total/3).toFixed(2) : 0
+  console.log('satitics'+ total)
 
-const Part = (props)=>{
-  console.log(props)
-  return(
-    <div>
-       <p>
-         {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-   console.log(props.content)
-   const content = props.content
-  return (
-    <div>
-      <Part part={content[0].part} exercises={content[0].exercises}/>
-      <Part part={content[1].part} exercises={content[1].exercises}/>
-      <Part part={content[2].part} exercises={content[2].exercises}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
- 
-  const total = props.content.map(c => c.exercises).reduce((valorAnterior, valorActual) => {
-    return valorAnterior + valorActual;
-  }, 0);
-  console.log(total)
+  const positive =  total > 0 ? (good*100/total).toFixed(2) + "%" : 0
+  if (total > 0) {
     return (
       <div>
-        <p>Number of exercises {total} </p>
+        <h1>Statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="total" value={total}/>
+            <StatisticLine text="average" value={average}/>
+            <StatisticLine text="positive" value={positive}/> 
+          </tbody>
+        </table>
+        
       </div>
     )
+  }else{
+    return(
+        <div>
+          <strong><p>Statistics</p></strong>
+        <p>No feedback Give</p>
+      </div>
+  )
+    
   }
+  
+}
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text} </button>
+
+const Header = ({text}) => <h1>{text}</h1>
 
 
 
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const content = [
-    { part: 'Fundamentals of React', exercises: 10},
-    { part: 'Using props to pass data', exercises: 7 },
-    { part: 'State of a component', exercises: 14 },
-  ]
- 
+
+    // 1.6: unicafe, paso 1
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    console.log('rendering...', good)
+    const increaseGood = () => {
+      const updatedGood = good + 1
+      console.log('increasing Good', updatedGood)
+      setGood(updatedGood)
+    }
+  
+    
+    console.log('rendering...', neutral)
+    const increaseNeutral = () => {
+      const updatedNeutral = neutral + 1
+      console.log('increasing Neutral,', updatedNeutral)
+      setNeutral(updatedNeutral)
+    }
+
+     
+    console.log('rendering...', bad)
+    const increaseBad = () => {
+      const updatedBad = bad + 1
+      console.log('increasing bad', updatedBad)
+      setBad(updatedBad)
+    }
+
+    
+
   return (
     <div>
-        
-      <Header course={course} />
-      <Content content = {content}/>
-      <Total content={content} />
+      
+      <Header text='Give feedback'/>
+      <Button onClick={increaseGood} text='Good'/> 
+      <Button onClick={increaseNeutral} text='Neutral'/>
+      <Button onClick={increaseBad} text='Bad'/>
+      <br/>
+
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+      
+     
+     
     </div>
   )
 }
